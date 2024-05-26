f0fc785bfcfc1cef86aee4b657812e5241440e45
Himanshu Kothari <hkothari247@gmail.com>
2024-05-26 10:21:26 +0530

    Changes : dropdown useRef, /logout route and function

--- frontend/_components/Top_Nav.tsx
+++ frontend/_components/Top_Nav.tsx
@@ -1,13 +1,29 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 
 export default function Top_Nav() {
     const router = useRouter();
     const location = router.pathname;
     const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
-    console.log(location)
+    
+    
+    const dropdownRef = useRef<any>(null);
+
+    useEffect(() => {
+        function handleClickOutside(event : any) {
+            if (dropdownRef.current && !dropdownRef?.current?.contains(event.target)) {
+                setIsDropdownOpen(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [dropdownRef]);
+
     return (
         <>
             <nav className="flex items-center justify-between px-4 h-[70px] bg-[#1d1f24] w-full">
@@ -22,9 +38,9 @@ export default function Top_Nav() {
                     <IoIosArrowDown color="white" />
                     {isDropdownOpen && (
                         <>
-                            <div className="bg-[#22242a] absolute top-[50px] w-[160px] right-0 text-sm p-3 border border-opacity-40 border-slate-300 rounded-lg">
-                                <Link href="logout" className="p-1 px-3 hover:bg-[#2f3139] rounded-[4px]">Profile</Link>
-                                <Link href="logout" className="p-1 px-3 hover:bg-[#2f3139] rounded-[4px]">Logout</Link>
+                            <div ref={dropdownRef} className="bg-[#22242a] absolute top-[50px] w-[160px] right-0 text-sm p-3 border border-opacity-40 border-slate-300 rounded-lg flex flex-col">
+                                <Link href="/profile" className="p-1 px-3 hover:bg-[#2f3139] rounded-[4px]">Profile</Link>
+                                <Link href="/logout" className="p-1 px-3 hover:bg-[#2f3139] rounded-[4px]">Logout</Link>
                             </div>
                         </>
                     )
---
+++ frontend/pages/logout.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { auth } from './firebase_store';
+import { useRouter } from 'next/router';
+
+export default function Logout() {
+    const router = useRouter();
+    
+    useEffect(() => {
+        const logout = async () => {
+            try {
+                await auth.signOut();
+                router.push('/login');
+            } catch (error) {
+                console.error('Error during logout', error);
+            }
+        };
+        logout();
+    }, []);
+
+    return <></>;
+}
