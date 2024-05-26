daea65143fbf572333d55cd4b41fb87c6fa3a270
Himanshu Kothari <hkothari247@gmail.com>
2024-05-26 10:54:56 +0530

    Changes : Title added

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <RecoilRoot>
+      <Head>
+        <title>Future Fund</title>
+      </Head>
       <Toaster toastOptions={{ duration: 1200 }} />
       <Component {...pageProps} />
     </RecoilRoot>
